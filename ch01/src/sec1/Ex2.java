8b3731052e28b1bd416852ad00adcc418b149a70
kyeonj <k_yeonj@naver.com>
2022-09-01 17:44:43 +0900

    add java Variable

--- ch01/src/sec1/Ex2.java
+++ ch01/src/sec1/Ex2.java
@@ -1,8 +1,8 @@
 package sec1;
 
 // 기능 클래스
-public class Ex2 {
-	public void methodl(){
-			System.out.println("하이~! 예제2~!");
+public class Ex2 {	//Ex2:클래스명
+	public void methodl(){	//methodl: 메서드명
+			System.out.println("하이~! 예제2~!");	//실행문장 끝에는 꼭 ;(세미콜론) 코드가또시작됨.
 	}
 }
--- ch01/src/sec1/Ex3.java
+++ ch01/src/sec1/Ex3.java
@@ -1,11 +1,15 @@
 package sec1;
 //저장 클래스
 public class Ex3 {
+	//멤버 필드 = 해당 클래스의 지역 변수
 	private String name;  //학생명
 	final static double PI = 3.14;
 	private int kor;	//국어점수
 	private int eng;	//영어점수
 	private int mat;	//수학점수
+	
+//	private String res;
+	//멤버 메서드= 해당 클래스의 기능(처리하고자하는 일)을 나타냄   컨트롤+/ 누르면 한줄각주생성됨.
 	public String getName() {
 		return name;
 	}
