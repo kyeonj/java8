d8c79a1f9cd4eecf4fdfbc57b8aff264b62a968d
kyeonj <k_yeonj@naver.com>
2022-09-14 17:30:51 +0900

    java looping

---
+++ ch04/src/sec3/Loop3.java
@@ -0,0 +1,23 @@
+package sec3;
+
+public class Loop3 {
+
+	public static void main(String[] args) {
+/*		반복변수=초기값;			1
+		do{
+			실행문장;			2
+			증감식;			3
+		} while(조건식);		4
+	1 -> 2 -> 3 -> 4 실행되다가 조건이 만족하는 동안 2 -> 3 -> 4 반복 수행함	
+	그러므로 실행부터 하고, 조건을 나중에 비교하므로 최서 1번이상 실행함.
+
+*/			
+		int a=10, tot=0;
+		do{
+			tot=tot+a;
+			a++;
+		}while(a<10);
+		System.out.println("a="+a+", tot="+tot);
+	}
+
+}
---
+++ ch04/src/sec3/Loop4.java
@@ -0,0 +1,26 @@
+package sec3;
+
+public class Loop4 {
+
+	public static void main(String[] args) {
+		//무한루프 : 조건이 항상 만족하여 끝도 없이 반복수행하게됨
+		int a=0, tot=0;
+		for(;;){
+			a++;
+			tot+=a; //tot=tot+a
+		}
+		
+/*		int n=0; 
+		int hap=0;
+		while(true) {
+			System.out.println("합계");
+		}
+*/
+		
+/*		for(int n=10;n>=9;n++){
+			
+		}
+*/		
+	}
+
+}
